ef40a481902e5826430947d8fc614a6c5bb4782d
Chinazor Akah <chinazor.akah@gmail.com>
2022-07-01 01:32:59 -0700

    Fast alphabet

---
+++ 0x02-python-import_modules/string.py
@@ -0,0 +1,16 @@
+def ascii_uppercase():
+
+# uppercase 
+
+    for c in range(65, 91): 
+
+        print(chr(c), end = " "); 
+
+  
+
+    print(""); 
+
+  
+# Driver code 
+print("ascii_uppercase");
+ascii_uppercase(); 
